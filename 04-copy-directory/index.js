edd0706e136fb618d13d10b8335a84f29c16681e
Katerina <comina.ru@gmail.com>
2022-05-22 16:29:38 +0300

    refactor: update  04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,33 +9,23 @@ let copyDir = function(folder, copyFolder) {
     .then(function() {
 
       fs.readdir(path.join(__dirname, copyFolder), (err, files) => {
-        if (err) {
-          console.log(err);
-        } else {
-          files.forEach((file) => {
-
-            fs.unlink(path.join(path.join(__dirname, copyFolder), file), (err) => {
-              if (err) throw err;
-            });
-
+        if(err) throw err;
+        files.forEach((file) => {
+          fs.unlink(path.join(path.join(__dirname, copyFolder), file), (err) => {
+            if (err) throw err;
           });
-        }
+        });
       });
 
       fs.readdir(path.join(__dirname, folder), (err, files) => {
-        if (err) {
-          console.log(err);
-        } else {
-          files.forEach((file) => {
-
-            fsPromises.copyFile(path.join(__dirname, folder, file), path.join(__dirname, copyFolder, file))
-              .then(function() {})
-              .catch(function(error) {
-                console.log(error);
-              });
-
-          });
-        }
+        if(err) throw err;
+        files.forEach((file) => {
+          fsPromises.copyFile(path.join(__dirname, folder, file), path.join(__dirname, copyFolder, file))
+            .then(function() {})
+            .catch(function(error) {
+              console.log(error);
+            });
+        });
       });
 
     }).catch(function() {
