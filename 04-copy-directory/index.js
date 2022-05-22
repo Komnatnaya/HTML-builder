b8da53afba7180f7a8312d5b6360a2d10a713d08
Katerina <comina.ru@gmail.com>
2022-05-22 14:13:48 +0300

    feat: add  04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,51 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const fsPromises = fs.promises;
+// const filesFolder = path.join(__dirname, 'files');
+// const copyFolder = path.join(__dirname, 'files-copy');
+
+let copyDir = function(folder, copyFolder) {
+  fsPromises.mkdir(path.join(__dirname, copyFolder), { recursive: true })
+    .then(function() {
+      console.log('Directory created successfully');
+
+      fs.readdir(path.join(__dirname, copyFolder), (err, files) => {
+        if (err) {
+          console.log(err);
+        } else {
+          files.forEach((file) => {
+
+            fs.unlink(path.join(path.join(__dirname, copyFolder), file), (err) => {
+              if (err) throw err;
+            });
+
+          });
+        }
+      });
+
+      fs.readdir(path.join(__dirname, folder), (err, files) => {
+        if (err) {
+          console.log(err);
+        } else {
+          files.forEach((file) => {
+
+            fsPromises.copyFile(path.join(__dirname, folder, file), path.join(__dirname, copyFolder, file))
+              .then(function() {
+                console.log('File Copied');
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+
+          });
+        }
+      });
+
+    }).catch(function() {
+      console.log('failed to create directory');
+    });
+};
+
+copyDir('files', 'files-copy');
