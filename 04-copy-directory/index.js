78deee61b84b87ca7adff97018ad0b5bf708b646
Katerina <comina.ru@gmail.com>
2022-05-22 15:04:34 +0300

    refactor: update  04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,13 +3,10 @@ const fs = require('fs');
 const path = require('path');
 
 const fsPromises = fs.promises;
-// const filesFolder = path.join(__dirname, 'files');
-// const copyFolder = path.join(__dirname, 'files-copy');
 
 let copyDir = function(folder, copyFolder) {
   fsPromises.mkdir(path.join(__dirname, copyFolder), { recursive: true })
     .then(function() {
-      console.log('Directory created successfully');
 
       fs.readdir(path.join(__dirname, copyFolder), (err, files) => {
         if (err) {
@@ -32,9 +29,7 @@ let copyDir = function(folder, copyFolder) {
           files.forEach((file) => {
 
             fsPromises.copyFile(path.join(__dirname, folder, file), path.join(__dirname, copyFolder, file))
-              .then(function() {
-                console.log('File Copied');
-              })
+              .then(function() {})
               .catch(function(error) {
                 console.log(error);
               });
