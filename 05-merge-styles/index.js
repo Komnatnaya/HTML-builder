f10ca3662df6a3ecd4e54a6115c728c8c1733b7a
Katerina <comina.ru@gmail.com>
2022-05-22 16:27:25 +0300

    refactor: update  05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,25 +6,19 @@ fs.open(path.join(__dirname, 'project-dist', 'bundle.css'), 'w', (err) => {
 });
 
 fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach((file) => {
-      if (path.extname(file).slice(1) === 'css') {
-        fs.readFile(path.join(__dirname, 'styles', file), 'utf8', (err, data) => {
-          if (err) {
-            console.log(err);
-          } else {
-            fs.appendFile(
-              path.join(__dirname, 'project-dist', 'bundle.css'),
-              `\n${data}`,
-              err => {
-                if (err) throw err;
-              }
-            );
+  if(err) throw err;
+  files.forEach((file) => {
+    if (path.extname(file).slice(1) === 'css') {
+      fs.readFile(path.join(__dirname, 'styles', file), 'utf8', (err, data) => {
+        if(err) throw err;
+        fs.appendFile(
+          path.join(__dirname, 'project-dist', 'bundle.css'),
+          `\n${data}`,
+          err => {
+            if (err) throw err;
           }
-        });
-      }
-    });
-  }
+        );
+      });
+    }
+  });
 });
