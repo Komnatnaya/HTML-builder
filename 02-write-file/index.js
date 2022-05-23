cd557f45840fd15a78d898c17c429f679e084152
Katerina <comina.ru@gmail.com>
2022-05-23 20:15:41 +0300

    refactor: update 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,31 +1,61 @@
 const fs = require('fs');
 const path = require('path');
 const { stdin, stdout} = process;
+const readline = require('readline');
+
 const textFile = path.join(__dirname, 'text.txt');
+const stream = fs.createWriteStream(textFile);
 
-fs.open(textFile, 'w', (err) => {
-  if(err) throw err;
-  stdout.write('Введите текст\n');
-});
+// fs.open(textFile, 'w', (err) => {
+//   if(err) throw err;
+//   stdout.write('Введите текст\n');
+// });
+stdout.write('Введите, пожалуйста, текст.\n');
 
-stdin.on('data', (data) => {
-  if (data.toString().trim() == 'exit') {
-    process.exit();
+const rl = readline.createInterface({input: stdin, output: stdout});
+
+rl.on('line', (input) => {
+  if (input.toString().trim().toLowerCase() == 'exit') {
+    // process.exit();
+    stdout.write('Файл создан. Удачи!');
+    rl.close();
   }
-
-  fs.appendFile(
-    textFile,
-    data,
-    err => {
-      if (err) throw err;
-    }
-  );
+  stream.write(`${input}\n`);
 });
 
-process.on('SIGINT', () => {
-  process.exit();
-});
-
-process.on('exit', () => {
+rl.on('SIGINT', () => {
   stdout.write('Файл создан. Удачи!');
+  rl.close();
 });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(
+//     `Thank you for your valuable feedback: ${answer}`
+//   );
+
+//   rl.close();
+// });
+
+// stdin.on('data', (data) => {
+//   if (data.toString().trim().toLowerCase() == 'exit') {
+//     process.exit();
+//   }
+
+//   // fs.appendFile(
+//   //   textFile,
+//   //   data,
+//   //   err => {
+//   //     if (err) throw err;
+//   //   }
+//   // );
+//   stream.write(data);
+// });
+
+// process.on('SIGINT', () => {
+//   process.exit();
+// });
+
+// process.on('exit', () => {
+//   stdout.write('Файл создан. Удачи!');
+// });
